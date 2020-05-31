c1dfc954ff9f0333b2be3dab99a425e233e8e5bd
exerex <exerex11@gmail.com>
2020-05-30 23:34:48 -0300

    initial commit

---
+++ audio.py
@@ -0,0 +1,31 @@
+import pyaudio
+import wave
+
+def play_audio(filename):
+    chunk = 1024
+    wf = wave.open(filename, 'rb')
+    pa = pyaudio.PyAudio()
+
+    stream = pa.open(
+        format = pa.get_format_from_width(wf.getsampwidth()),
+        channels = wf.getnchannels(),
+        rate = wf.getframerate(),
+        outout = True
+    )
+
+    data_stream = wf.readframes(chunk)
+
+    while data_stream:
+        stream.write(data_stream)
+        data_stream = wf.readframes(chunk)
+
+    stream.close()
+    pa.terminate()
+
+play_audio("./audio/promise.mp3")
+
+# file = pyglet.resource.media('audio/promise.mp3')
+# file.play()
+
+# pyglet.app.run()
+
